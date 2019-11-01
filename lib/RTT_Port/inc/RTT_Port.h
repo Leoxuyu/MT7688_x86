96e8bcd9ddb7811579639fb727a41d01767752d3
Leoxuyu <1179317967@qq.com>
2019-11-01 16:31:17 +0800

    add LSSDP and libmodbus and samples

---
+++ lib/RTT_Port/inc/RTT_Port.h
@@ -0,0 +1,13 @@
+/*
+ * RTT_Port.h
+ *
+ *  Created on: 2019年10月30日
+ *      Author: work
+ */
+
+#ifndef LIB_RTT_PORT_INC_RTT_PORT_H_
+#define LIB_RTT_PORT_INC_RTT_PORT_H_
+
+
+
+#endif /* LIB_RTT_PORT_INC_RTT_PORT_H_ */
---
+++ lib/Sample/inc/lssdp_sample.h
@@ -0,0 +1,13 @@
+/*
+ * lssdp_sample.h
+ *
+ *  Created on: 2019年10月31日
+ *      Author: work
+ */
+
+#ifndef LIB_SAMPLE_INC_LSSDP_SAMPLE_H_
+#define LIB_SAMPLE_INC_LSSDP_SAMPLE_H_
+
+void lssdp_sample(void);
+
+#endif /* LIB_SAMPLE_INC_LSSDP_SAMPLE_H_ */
